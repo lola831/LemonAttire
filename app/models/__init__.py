bc2fc4011bffba3de5aa09bac6e2bd9f66f9d8b9
lola <keepthechange2021@gmail.com>
2023-07-20 08:33:13 -0700

    added style-item model to init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -3,10 +3,10 @@ from .category import Category
 from .favorite import Favorite
 from .order_item import OrderItem
 from .order import Order
-from .product_styles import product_styles
 from .product_type import ProductType
 from .product import Product
 from .review import Review
+from .style_item import StyleItem
 from .style import Style
 from .user import User
 from .db import environment, SCHEMA
