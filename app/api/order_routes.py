4bbbff23c721b014205bff98775e2004c8f64ce1
lola <keepthechange2021@gmail.com>
2023-07-23 12:48:47 -0700

    completed order and style api routes

--- app/api/order_routes.py
+++ app/api/order_routes.py
@@ -1,7 +1,8 @@
-from flask import Blueprint, request
+from flask import Blueprint, request, jsonify
 from flask_login import login_required, current_user
 from app.models import Order, db
 from app.forms import OrderForm
+from datetime import datetime
 # from app.api import validation_errors_to_error_messages #need init file in api??
 
 order_routes = Blueprint('orders', __name__)
@@ -19,7 +20,7 @@ def validation_errors_to_error_messages(validation_errors):
 
 # GET ALL ORDERS
 @order_routes.route('/')
-def orders():
+def all_orders():
     """
     Query for all orders and returns them in a list of order dictionaries
     """
@@ -40,7 +41,7 @@ def last_order():
 # GET ALL USER'S ORDERS
 @order_routes.route('/current')
 @login_required
-def order():
+def user_orders():
     """
     Query for all orders by user id and returns them in a list of order dictionaries ordered by most recent first
     """
@@ -66,3 +67,38 @@ def create_order():
         db.session.commit()
         return order.to_dict()
     return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+# EDITS AN ORDER
+@order_routes.route('/<int:order_id>', methods=['PUT'])
+@login_required
+def edit_order(order_id):
+    """
+    Edits an order
+    """
+    order = Order.query.get(order_id)
+    if order is None:
+        return jsonify({'error': 'Order not found'}), 404
+    if current_user.id is not order.user_id:
+        return jsonify({'error': 'You are not authorized to edit this post'}), 400
+
+    form = OrderForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        form.populate_obj(order)
+        order.updated_at = datetime.utcnow()
+        db.session.commit()
+        return order.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+# DELETE AN ORDER
+@order_routes.route('/<int:order_id>', methods=['DELETE'])
+@login_required
+def delete_order(order_id):
+    order = Order.query.get(order_id)
+    if order is None:
+        return jsonify({'error': 'Order not found'}), 404
+    if current_user.id is not order.user_id:
+        return jsonify({'error': 'You are not authorized to edit this post'}), 400
+    db.session.delete(order)
+    db.session.commit()
+    return {'message': 'Your order has been cancelled.'}
---
+++ app/api/styles_routes.py
@@ -0,0 +1,90 @@
+from flask import Blueprint, request, jsonify
+from flask_login import login_required, current_user
+from app.models import Style, db
+from app.forms import StyleForm
+from datetime import datetime
+
+style_routes = Blueprint('styles', __name__)
+
+def validation_errors_to_error_messages(validation_errors):
+    """
+    Simple function that turns the WTForms validation errors into a simple list
+    """
+    errorMessages = []
+    for field in validation_errors:
+        for error in validation_errors[field]:
+            errorMessages.append(f'{field} : {error}')
+    return errorMessages
+
+# GET ALL STYLES
+@style_routes.route('/')
+def all_styles():
+    """
+    Query for all styles and returns them in a list of style dictionaries
+    """
+    styles = Style.query.all()
+    return {'styles': [style.to_dict() for style in styles]}
+
+# GET ALL USER'S STYLES
+@style_routes.route('/current')
+@login_required
+def user_styles():
+    """
+    Query for all styles by user id and returns them in a list of style dictionaries ordered by most recent first
+    """
+    styles = Style.query.filter_by(user_id=current_user.id).order_by(Style.created_at) #.desc() ????
+    return {'user_styles': [style.to_dict() for style in styles]}
+
+# CREATE NEW STYLE
+@style_routes.route('/', methods=['POST'])
+@login_required
+def create_style():
+    """
+    Creates a new style
+    """
+    form = StyleForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        style = Style(
+            title=form.data['title'],
+            user_id=current_user.id
+        )
+        db.session.add(style)
+        db.session.commit()
+        return style.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+# EDITS A STYLE
+@style_routes.route('/<int:style_id>', methods=['PUT'])
+@login_required
+def edit_style(style_id):
+    """
+    Edits a style
+    """
+    style = Style.query.get(style_id)
+    if style is None:
+        return jsonify({'error': 'Style not found'}), 404
+    if current_user.id is not style.user_id:
+        return jsonify({'error': 'You are not authorized to edit this style'}), 400
+
+    form = StyleForm()
+    form['csrf_token'].data = request.cookies['csrf_token']
+    if form.validate_on_submit():
+        form.populate_obj(style)
+        style.updated_at = datetime.utcnow()
+        db.session.commit()
+        return style.to_dict()
+    return {'errors': validation_errors_to_error_messages(form.errors)}, 401
+
+# DELETE A STYLE
+@style_routes.route('/<int:style_id>', methods=['DELETE'])
+@login_required
+def delete_style(style_id):
+    style = Style.query.get(style_id)
+    if style is None:
+        return jsonify({'error': 'Style not found'}), 404
+    if current_user.id is not style.user_id:
+        return jsonify({'error': 'You are not authorized to edit this style'}), 400
+    db.session.delete(style)
+    db.session.commit()
+    return {'message': 'Your style has been deleted.'}
