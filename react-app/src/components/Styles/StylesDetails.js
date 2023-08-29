6dc5f90091855e75fda1943649366ea5c15e42bb
lola <keepthechange2021@gmail.com>
2023-08-29 11:01:10 -0700

    fixed small bug in edit style title, when user changes title but then x's out, title remains unchanged

--- react-app/src/components/Styles/StylesDetails.js
+++ react-app/src/components/Styles/StylesDetails.js
@@ -21,8 +21,10 @@ function StylesDetails() {
   console.log("STYLE: ", style)
   console.log("EDIT: ", edit)
   console.log("EROOR: ", errors)
+  console.log("TITLE", title)
 
   useEffect(() => {
+    console.log("in use eff")
     dispatch(getUserStyles())
   }, [dispatch]);
 
@@ -39,17 +41,32 @@ function StylesDetails() {
     setIsDeleted(true)
   }
 
+  const cancelEdit = () => {
+    console.log("title!", title)
+    console.log("style title!", style.title)
+    setTitle(style.title)
+    setEdit(false)
+  }
+
+  console.log("before handle submit", errors)
+
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    if (title === style.title) setEdit(false)
+    if (title === style.title) return setEdit(false);
 
-    console.log("errors: ", errors)
-    if (errors.title) errors.title = "";
+
+
+    console.log("out")
+
+    // console.log("errors: ", errors)
+    // if (errors.title) errors.title = "";
 
     const data = await dispatch(modifyStyle(style.id, { title }))
     if (data.errors) {
+      console.log("here")
       setErrors(data.errors[0])
+      console.log("newwwww error: ", errors)
 
     } else {
       setEdit(false)
@@ -86,8 +103,8 @@ function StylesDetails() {
                     required
                   />
                 </div>
-                {errors.title && (<span className="error">{errors.title}</span>)}
-                <i className="fa-solid fa-x cancel-title" onClick={() => setEdit(false)}></i>
+                {errors.title && (<span className="error"> *{errors.title}</span>)}
+                <i className="fa-solid fa-x cancel-title" onClick={cancelEdit}></i>
                 <button className='store-button style-submit-title' type='submit'>save title</button>
                 {/* <button className='store-button cancel-title' onClick={() => setEdit(false)}>cancel</button> */}
               </form>
@@ -96,15 +113,15 @@ function StylesDetails() {
         </div>
         <div className='main-style-details-container'>
           <div className='style-items-display'>
-          {
-            style.styleItems.length ? (
-              <DisplayStyleItems productValues={style.styleItems} />
-            ) : (
-              <div className='style-is-empty'>Your style is empty,
-                <Link className='add-items-link' to="/"> shop and add some items!</Link>
-              </div>
-            )
-          }
+            {
+              style.styleItems.length ? (
+                <DisplayStyleItems productValues={style.styleItems} />
+              ) : (
+                <div className='style-is-empty'>Your style is empty,
+                  <Link className='add-items-link' to="/"> shop and add some items!</Link>
+                </div>
+              )
+            }
           </div>
 
           <div>
