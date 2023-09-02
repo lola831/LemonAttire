8c0d13f3901a579e63c6b15d1a3b4f2ca943db04
lola <keepthechange2021@gmail.com>
2023-09-02 13:33:01 -0700

    added shipping address, payment details, and order summary to shipping page

--- react-app/src/components/ShippingPage/index.js
+++ react-app/src/components/ShippingPage/index.js
@@ -1,10 +1,13 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { Redirect } from 'react-router-dom';
+import "./ShippingPage.css"
+
 function ShippingPage() {
 
     const dispatch = useDispatch();
     const user = useSelector(state => state.session.user)
+    const order = useSelector(state => state.orders)
     const [address, setAddress] = useState("");
     const [city, setCity] = useState("");
     const [state, setState] = useState("")
@@ -22,87 +25,131 @@ function ShippingPage() {
         e.PreventDefault();
         const data = await dispatch()
     }
+    console.log("order", order)
 
-  return (
-    <div>
-    <div>Shipping Address</div>
-    <form>
-       <label>
-        Street Address
-       </label>
-       <input
-        type="text"
-        value={address}
-        onChange={(e) => setAddress(e.target.value)}
-        required>
-       </input>
+    if (Object.keys(order).length) {
+        return (
+            <div className='shipping-container'>
 
-       <label>
-        City
-       </label>
-       <input
-        type="text"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        required>
-       </input>
+                <form className='shipping-form'>
 
-       <label>
-        State
-       </label>
-       <input
-        type="text"
-        value={state}
-        onChange={(e) => setState(e.target.value)}
-        required>
-       </input>
+                    <div className='shipping-name'>Shipping Address</div>
 
-       <label>
-        ZIP Code
-       </label>
-       <input
-        type="text"
-        value={zipcode}
-        onChange={(e) => setZipcode(e.target.value)}
-        required>
-       </input>
+                    <div className='shipping-input'>
+                        <label>
+                            Street Address:
+                        </label>
+                        <input
+                            className='ship-input long-input'
+                            type="text"
+                            value={address}
+                            onChange={(e) => setAddress(e.target.value)}
+                            required>
+                        </input>
+                    </div>
 
-       <div>Payment Details</div>
+                    <div className='shipping-input'>
+                        <label>
+                            City:
+                        </label>
+                        <input
+                            className='ship-input long-input'
+                            type="text"
+                            value={city}
+                            onChange={(e) => setCity(e.target.value)}
+                            required>
+                        </input>
+                    </div>
 
-       <label>
-        Card Number
-       </label>
-       <input
-        type="text"
-        value={zipcode}
-        onChange={(e) => setZipcode(e.target.value)}
-        required>
-       </input>
+                    <div className='shipping-input'>
+                        <label>
+                            State:
+                        </label>
+                        <input
+                            className='ship-input short-input'
+                            type="text"
+                            value={state}
+                            onChange={(e) => setState(e.target.value)}
+                            required>
+                        </input>
+                    </div>
 
-       <label>
-       Expiration Date (MM/YY)
-       </label>
-       <input
-        type="text"
-        value={expDate}
-        onChange={(e) => setExpDate(e.target.value)}
-        required>
-       </input>
+                    <div className='shipping-input zip'>
+                        <label>
+                            ZIP Code:
+                        </label>
+                        <input
+                            className='ship-input short-input'
+                            type="text"
+                            value={zipcode}
+                            onChange={(e) => setZipcode(e.target.value)}
+                            required>
+                        </input>
+                    </div>
 
-       <label>
-        Security Code (CVV)
-       </label>
-       <input
-        type="text"
-        value={cvv}
-        onChange={(e) => setCvv(e.target.value)}
-        required>
-       </input>
+                    <div className='payment-details'>Payment Details</div>
 
-    </form>
+                    <div className='shipping-input'>
+                        <label>
+                            Card Number:
+                        </label>
+                        <input
+                            className='ship-input long-input'
+                            type="text"
+                            value={cardnumber}
+                            onChange={(e) => setCardnumber(e.target.value)}
+                            required>
+                        </input>
+                    </div>
 
-    </div>
-  )
+                    <div className='shipping-input'>
+                        <label>
+                            Expiration Date (MM/YY):
+                        </label>
+                        <input
+                            className='ship-input short-input'
+                            type="text"
+                            value={expDate}
+                            onChange={(e) => setExpDate(e.target.value)}
+                            required>
+                        </input>
+                    </div>
+
+                    <div className='shipping-input cvv'>
+                        <label>
+                            Security Code (CVV):
+                        </label>
+                        <input
+                            className='ship-input short-input'
+                            type="text"
+                            value={cvv}
+                            onChange={(e) => setCvv(e.target.value)}
+                            required>
+                        </input>
+                    </div>
+
+                </form>
+
+                <div className='order-sum-box'>
+                    <div className="order-summary-container">
+                        <div className="order-summary">Order Summary</div>
+                        <div className="order-price">
+                            subtotal:<span>${order.price}.00</span>
+                        </div>
+                        <div className="order-price">
+                            tax:<span>${order.tax.toFixed(2)}</span>
+                        </div>
+                        <div className="order-price total">
+                            total price:<span>${order.totalPrice.toFixed(2)}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    } else {
+        return <div>Loading...</div>
+    }
+
 }
 
 export default ShippingPage
