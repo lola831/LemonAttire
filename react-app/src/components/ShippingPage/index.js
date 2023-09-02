db3516479b8149dd4b4b8dd01533ef2536b15633
lola <keepthechange2021@gmail.com>
2023-09-02 15:19:17 -0700

    added css to confirmation order page

--- react-app/src/components/ShippingPage/index.js
+++ react-app/src/components/ShippingPage/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { Redirect } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import { submitOrder } from '../../store/orders';
 import "./ShippingPage.css"
 
@@ -25,12 +25,16 @@ function ShippingPage() {
     const handleSubmit = () => {
         dispatch(submitOrder(order.id))
     }
-    console.log("order", order)
+    console.log("user", user)
 
     if (!order) {
         return (
-            <div>
-                Youre order has been placed!
+            <div className='order-conf-container'>
+                <div className='order-conf-title'>Order Confirmation</div>
+                <div>Order Number: #8485950505</div>
+                <div>{user.firstName}, thank you for your order!</div>
+                <div className='conf-msg'>We've received your order and will contact you as soon as your package is shipped. You'll soon receive a confirmation email in your inbox.</div>
+                <Link className='new-order' to="/">Place a new order!</Link>
             </div>
         )
     }
