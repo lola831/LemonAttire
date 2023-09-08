37b3abeca2b1636ccb6d4b81eeb2fe1d2ee23a4d
lola <keepthechange2021@gmail.com>
2023-09-08 13:39:36 -0700

    changed versions folder

--- migrations/versions/24d17c259fb5_.py
+++ migrations/versions/24d17c259fb5_.py
@@ -1,13 +1,16 @@
 """empty message
 
 Revision ID: 24d17c259fb5
-Revises: 
+Revises:
 Create Date: 2023-09-08 11:40:05.264012
 
 """
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '24d17c259fb5'
